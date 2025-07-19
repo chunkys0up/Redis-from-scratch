96e3eadfd9a7924ba829628c369b2a1e458b9d31
Patheit <andrewtnguyen0106@gmail.com>
2025-07-19 11:56:51 -0700

    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -48,9 +48,13 @@ int main(int argc, char **argv) {
   std::cout << "Waiting for a client to connect...\n";
 
   std::cout << "Logs from your program will appear here!\n";
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   
+  std::string response = "+PONG\r\n";
+  send(client_fd, response.c_str(), response.size(), 0);
+
+  close(client_fd);
   close(server_fd);
 
   return 0;
