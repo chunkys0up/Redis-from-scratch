dd0ee29d954c7dcda05801f660f1b361720d2417
Patheit <andrewtnguyen0106@gmail.com>
2025-07-20 16:20:25 -0700

    [any message]

--- src/commands/redis_parser.hpp
+++ src/commands/redis_parser.hpp
@@ -5,7 +5,7 @@
 #include <string>
 
 // for GET and SET commands
-inline unordered_map<string, string> redisMap;
+inline unordered_map<std::string, std::string> redisMap;
 
 // check for ping, if not then check for echo command
 void parse_redis_command(char *buffer, int client_fd);
